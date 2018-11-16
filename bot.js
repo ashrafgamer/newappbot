f37088e580722f755f4d8c7417e90fb6c21a308f
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-16 17:33:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,6 +84,30 @@ if (!channel) return;
 channel.send({embed : embed});
 });
 
+ 
+
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('%bc')) {
+          if (!args[0]) {
+message.channel.send("**%bc <message>**");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
