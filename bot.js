da62bd9216a1bd375b634556e6504379e0dc00c6
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-16 10:40:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,15 @@ client.on('ready', () => {
 client.user.setGame(`"%help"`,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`-help | On 20 Servers`,'https://www.twitch.tv/v5bz');
 });          
 
+client.on('message', message => {
+     if (message.content === "") {
+message.author.send("**%help**" + `  **
+-ash
+ 
+
+**`);
+    }
+});     
 
 
 // THIS  MUST  BE  THIS  WAY
