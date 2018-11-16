46be6579403c02ad8e05b8b95c07182823a11ce1
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-16 10:41:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'السلام عليكم') {
+    	message.reply('عليكم السلام :heart:');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
