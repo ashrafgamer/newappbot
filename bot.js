4134af5f76683c8960d12f4d0c7d176594f680b2
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-16 15:48:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,9 +28,28 @@ if(!message.channel.guild) return;
   }
 });
  
-client.on('ready', () => {                           
-client.user.setGame(`"%help%`,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                              client.user.setGame(`-help | On 20 Servers`,'https://www.twitch.tv/v5bz');
-});          
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
 
 
 // THIS  MUST  BE  THIS  WAY
