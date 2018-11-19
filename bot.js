68186093530e4c5f892f33d81eabc4cdd02c0d99
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-19 18:38:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,26 +85,6 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-if (message.content.startsWith("kick")) {
-    var mention = message.mentions.members.first();
-    if(!mention) return message.channel.send("يجب منشن العضو");
-
-    mention.kick("By: " + message.author.tag);
-    
-    message.channel.send("تم أعطاء كيك الى : " + mention.tag);
-};
-});
-client.on('message', message => {
-if (message.content.startsWith("ban")) {
-    var mention = message.mentions.members.first();
-    if(!mention) return message.channel.send("يجب منشن العضو");
-
-    mention.ban("By: " + message.author.tag);
-    
-    message.channel.send("تم أعطاء باند الى : " + mention.tag);
-};
-});
 client.on('message', msg => {
   if (msg.author.bot) return;
   if (!msg.content.startsWith(prefix)) return;
